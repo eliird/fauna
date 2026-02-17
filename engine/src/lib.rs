3c4f597d4c833ee0e0f70facb17303f243e266b6
irdali <irdali1996@gmail.com>
2026-02-17 18:19:04 +0900

    switched to wgpu

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,9 +1,12 @@
 use wasm_bindgen::prelude::*;
-use web_sys::WebGl2RenderingContext as GL;
 
 #[wasm_bindgen]
 pub fn init(){
     // Step 1: get window and document
+    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor{
+        backends: wgpu::Backends::BROWSER_WEBGPU | wgpu::Backends::GL,
+        ..Default::default()
+    });
 
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
@@ -11,19 +14,11 @@ pub fn init(){
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into().unwrap();
 
-    // get a webGL2 context
+    let surface_target = wgpu::SurfaceTarget::Canvas(canvas);
+    let surface = instance.create_surface(surface_target).unwrap();
 
-    let gl: GL = canvas
-        .get_context("webgl2")
-        .unwrap()
-        .unwrap()
-        .dyn_into()
-        .unwrap();
 
-    // Step 4: set clear color to dark teal (r, g, b, a)
-    gl.clear_color(0.1, 0.2, 0.3, 1.0);
 
-    // Step 5: clear the screen
-    gl.clear(GL::COLOR_BUFFER_BIT);
+
 
 }
